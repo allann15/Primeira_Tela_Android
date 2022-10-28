0dc3dc0d4ffdafe8faf943c03ed8a4e897290d74
allann15 <allanarruda19@gmail.com>
2022-10-27 21:44:06 -0300

    primeira tela

---
+++ app/src/main/java/com/example/telalogin/FormCadastro.java
@@ -0,0 +1,19 @@
+package com.example.telalogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+        getSupportActionBar().hide();
+
+    }
+
+
+}
